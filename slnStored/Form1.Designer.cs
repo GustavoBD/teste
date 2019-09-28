5c782273fca48ceaa2f5a544e0dd7b89cb9b0165
default <default@dwefault>
2019-09-27 21:29:16 -0300

    Adicionar arquivos de projeto.

---
+++ slnStored/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace slnStored
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnPesquisar = new System.Windows.Forms.Button();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtIdade = new System.Windows.Forms.TextBox();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.btnCadastro = new System.Windows.Forms.Button();
+            this.btnAtualizar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(52, 29);
+            this.txtID.Multiline = true;
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(121, 27);
+            this.txtID.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nome";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Idade";
+            // 
+            // btnPesquisar
+            // 
+            this.btnPesquisar.Location = new System.Drawing.Point(179, 29);
+            this.btnPesquisar.Name = "btnPesquisar";
+            this.btnPesquisar.Size = new System.Drawing.Size(92, 27);
+            this.btnPesquisar.TabIndex = 2;
+            this.btnPesquisar.Text = "...";
+            this.btnPesquisar.UseVisualStyleBackColor = true;
+            this.btnPesquisar.Click += new System.EventHandler(this.btnPesquisar_Click);
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(52, 72);
+            this.txtNome.Multiline = true;
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(219, 27);
+            this.txtNome.TabIndex = 0;
+            // 
+            // txtIdade
+            // 
+            this.txtIdade.Location = new System.Drawing.Point(51, 115);
+            this.txtIdade.Multiline = true;
+            this.txtIdade.Name = "txtIdade";
+            this.txtIdade.Size = new System.Drawing.Size(121, 27);
+            this.txtIdade.TabIndex = 0;
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(323, 12);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(92, 27);
+            this.btnTest.TabIndex = 3;
+            this.btnTest.Text = "Testar Conexão";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // btnCadastro
+            // 
+            this.btnCadastro.Location = new System.Drawing.Point(323, 45);
+            this.btnCadastro.Name = "btnCadastro";
+            this.btnCadastro.Size = new System.Drawing.Size(92, 27);
+            this.btnCadastro.TabIndex = 3;
+            this.btnCadastro.Text = "Cadastrar";
+            this.btnCadastro.UseVisualStyleBackColor = true;
+            this.btnCadastro.Click += new System.EventHandler(this.btnCadastro_Click);
+            // 
+            // btnAtualizar
+            // 
+            this.btnAtualizar.Location = new System.Drawing.Point(323, 78);
+            this.btnAtualizar.Name = "btnAtualizar";
+            this.btnAtualizar.Size = new System.Drawing.Size(92, 27);
+            this.btnAtualizar.TabIndex = 3;
+            this.btnAtualizar.Text = "Editar";
+            this.btnAtualizar.UseVisualStyleBackColor = true;
+            this.btnAtualizar.Click += new System.EventHandler(this.btnAtualizar_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Location = new System.Drawing.Point(323, 111);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(92, 27);
+            this.btnExcluir.TabIndex = 3;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(323, 144);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(92, 27);
+            this.btnSair.TabIndex = 3;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(451, 192);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnAtualizar);
+            this.Controls.Add(this.btnCadastro);
+            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.btnPesquisar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtIdade);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.txtID);
+            this.Name = "Form1";
+            this.Text = "Cadastro Estoredi";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnPesquisar;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtIdade;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.Button btnCadastro;
+        private System.Windows.Forms.Button btnAtualizar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnSair;
+    }
+}
+
---
+++ slnStored/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace slnStored
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPesquisar_Click(object sender, EventArgs e)
+        {  try
+            {
+                string str = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=E:\\Programas\\aulaStoredProcedures\\slnStored\\slnStored\\BDAlunos.mdf;Integrated Security=True;Connect Timeout=30";
+                SqlConnection con = new SqlConnection(str);
+                SqlCommand cmd = con.CreateCommand();
+                cmd.CommandText = "Procurar";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@id", this.txtID.Text);
+                con.Open();
+                SqlDataReader rd = cmd.ExecuteReader();
+                if (rd.Read())
+                {
+                    txtID.Text = rd["id"].ToString();
+                    txtNome.Text = rd["nome"].ToString();
+                    txtIdade.Text = rd["idade"].ToString();
+                }
+                else
+                {
+                    MessageBox.Show("Registro não encontrado");
+                }
+           }
+            catch (Exception er)
+            {
+                MessageBox.Show(er.Message);
+            }
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string str = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=E:\\Programas\\aulaStoredProcedures\\slnStored\\slnStored\\BDAlunos.mdf;Integrated Security=True;Connect Timeout=30";
+                string query = "select * from Aluno ";
+                SqlConnection con = new SqlConnection(str);
+                SqlCommand cmd = new SqlCommand(query, con);
+                con.Open();
+                DataSet ds = new DataSet();
+                MessageBox.Show("Conectado com Sucesso!");
+                con.Close();
+            }
+            catch (Exception er)
+            {
+                MessageBox.Show(er.Message);
+            }
+        }
+
+        private void btnCadastro_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string str = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=E:\\Programas\\aulaStoredProcedures\\slnStored\\slnStored\\BDAlunos.mdf;Integrated Security=True;Connect Timeout=30";
+                SqlConnection con = new SqlConnection(str);
+                SqlCommand cmd = con.CreateCommand();
+                cmd.CommandText = "Inserir";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@nome", txtNome.Text);
+                cmd.Parameters.AddWithValue("@idade", txtIdade.Text);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Registro atualizado com sucesso!");
+                con.Close();
+                txtNome.Text = null;
+                txtIdade.Text = null;
+            }
+            catch (Exception er)
+            {
+                MessageBox.Show(er.Message);
+            }
+
+        }
+
+        private void btnAtualizar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string str = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=E:\\Programas\\aulaStoredProcedures\\slnStored\\slnStored\\BDAlunos.mdf;Integrated Security=True;Connect Timeout=30";
+                SqlConnection con = new SqlConnection(str);
+                SqlCommand cmd = con.CreateCommand();
+                cmd.CommandText = "Editar";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@id", this.txtID.Text);
+                cmd.Parameters.AddWithValue("@nome", this.txtNome.Text);
+                cmd.Parameters.AddWithValue("@idade", this.txtIdade.Text);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Registro atualizado com sucesso!");
+                con.Close();
+                txtNome.Text = null;
+                txtIdade.Text = null;
+                txtID.Text = null;
+            }
+            catch (Exception er)
+            {
+                MessageBox.Show(er.Message);
+            }
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string str = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=E:\\Programas\\aulaStoredProcedures\\slnStored\\slnStored\\BDAlunos.mdf;Integrated Security=True;Connect Timeout=30";
+                SqlConnection con = new SqlConnection(str);
+                SqlCommand cmd = con.CreateCommand();
+                cmd.CommandText = "Excluir";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@id", this.txtID.Text);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Registro excluido com sucesso!");
+                con.Close();
+                txtNome.Text = null;
+                txtIdade.Text = null;
+                txtID.Text = null;
+            }
+            catch (Exception er)
+            {
+                MessageBox.Show(er.Message);
+            }
+        }
+    }
+}
